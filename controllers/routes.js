8aa26ca9c8e17cf73c2a41cd52a334895b974dbc
xScoopy <69348168+xScoopy@users.noreply.github.com>
2021-10-11 14:10:23 -0500

    added quick test route to routes/ route

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,3 +1,12 @@
 require("dotenv/config")
 
 const express = require("express")
+const router = express.Router()
+const axios = require("axios")
+
+router.get("/", (req, res) => {
+    res.json({
+        router: "get"
+    })
+})
+
