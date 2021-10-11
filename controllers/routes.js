8c30b902c46948a5a04032667acf471afc671669
xScoopy <69348168+xScoopy@users.noreply.github.com>
2021-10-11 17:55:38 -0500

    began function for paginating json responses, but out of scope so putting in the icebox for later

--- controllers/routes.js
+++ controllers/routes.js
@@ -13,6 +13,7 @@ router.get("/", (req, res) => {
 
 //route to retrieve orders
 router.get("/orders", (req, res) => {
+    let allOrders = {}
     //axios options
     const options = {
         method: "get", 
@@ -28,6 +29,29 @@ router.get("/orders", (req, res) => {
         res.setHeader("Access-Control-Allow-Origin", "*")
         res.json(response.data)
     })
+    .catch((err) => {
+        throw err
+    })
+    // axios(options)
+    // .then((response) => {
+    //     allOrders = {
+    //         ...allOrders,
+    //         ...response.data
+    //     }
+    //     let next_page = response.data["pagination"]["hasNextPage"]
+    //     while(next_page){
+    //         options["url"] = response.data["pagination"]["nextPageUrl"]
+    //         axios(options)
+    //         .then((nextPage) => {
+    //             next_page = nextPage.data["pagination"]["hasNextPage"]
+    //             console.log(next_page)
+    //             allOrders = {
+    //                 ...allOrders,
+    //                 ...nextPage.data
+    //             }
+    //         })
+    //     }
+    // })
     .catch((err) => {
         throw err
     })
