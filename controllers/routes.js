6b6a1aa0e95e732036296830d8f8bce6d3db55a9
xScoopy <69348168+xScoopy@users.noreply.github.com>
2021-10-11 16:23:45 -0500

    added api call to retrieve order information and return in json format

--- controllers/routes.js
+++ controllers/routes.js
@@ -4,9 +4,32 @@ const express = require("express")
 const router = express.Router()
 const axios = require("axios")
 
+//quick test route
 router.get("/", (req, res) => {
     res.json({
         router: "get"
+    })
+})
+
+//route to retrieve orders
+router.get("/orders", (req, res) => {
+    //axios options
+    const options = {
+        method: "get", 
+        url: process.env.orders_url,
+        headers: {
+            "Authorization": process.env.secret_token,
+            "User-Agent": "Yker-App"
+        }
+    }
+    //axios get
+    axios(options)
+    .then((response) => {
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        res.json(response.data)
+    })
+    .catch((err) => {
+        throw err
     })
 })
 
