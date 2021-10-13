ed3f2d20d8cb90822090e013a1edc1a5f60a94c3
xScoopy <69348168+xScoopy@users.noreply.github.com>
2021-10-13 13:01:49 -0500

    cleaned up comments and failed attempts at gathering all orders from the paginated api

--- controllers/routes.js
+++ controllers/routes.js
@@ -9,6 +9,7 @@ router.get("/", (req, res) => {
     router: "get",
   });
 });
+
 // axios options
 const options = {
   method: "get",
@@ -19,7 +20,7 @@ const options = {
   },
 };
 const getAllOrders = async () => {
-  let allUrls = []
+  let allUrls = [];
   while (true) {
     console.log("inside the while");
     let res = await axios(options);
@@ -28,84 +29,26 @@ const getAllOrders = async () => {
       break;
     }
     let nextUrl = await res.data["pagination"]["nextPageUrl"];
-    allUrls.push(nextUrl)
+    allUrls.push(nextUrl);
     options["url"] = nextUrl;
-    
   }
-  return { 
-      urls: allUrls
-  }
+  return {
+    urls: allUrls,
+  };
 };
 //route to retrieve orders
 router.get("/orders", (req, res) => {
-//   const allUrls = await getAllOrders();
-//   res.json(allUrls)
+  //   const allUrls = await getAllOrders();
+  //   res.json(allUrls)
 
-  //axios options
-  const options = {
-      method: "get",
-      url: process.env.orders_url,
-      headers: {
-          "Authorization": process.env.secret_token,
-          "User-Agent": "Yker-App"
-      }
-  }
   axios(options)
-  .then((response) => {
-    res.json(response.data)
-  })
-
-  // let lastUrl = []
-  // while(options["url"] != null){
-  //     axios(options)
-  //     .then((res) => {
-  //         options["url"] = res.data["pagination"]["nextPageUrl"]
-  //         console.log(res.data["pagination"]["nextPageUrl"])
-  //     })
-  //     .catch((err) => {
-  //         console.log(err)
-  //     })
-  // }
-  //axios get
-  // axios(options)
-  // .then((response) => {
-  //     res.setHeader("Access-Control-Allow-Origin", "*")
-  //     res.json(response.data)
-  // })
-  // .catch((err) => {
-  //     throw err
-  // })
-  //attempt2
-  // axios(options)
-  // .then((response) => {
-  //     allOrders = {
-  //         ...allOrders,
-  //         ...response.data
-  //     }
-  //     options["url"] = response.data["pagination"]["nextPageUrl"]
-  //     console.log("right before while loop")
-  //     while(options["url"]){
-  //         console.log("inside while loop")
-  //         axios(options)
-  //         .then((nextPage) => {
-  //             console.log("inside then inside while loop")
-  //             allOrders = {
-  //                 ...allOrders,
-  //                 ...nextPage.data
-  //             }
-  //             console.log("right before changing url in while loop")
-  //             options["url"] = nextPage.data["pagination"]["nextPageUrl"]
-  //             console.log(options["url"])
-  //         })
-  //         .catch((err) => {
-  //             console.log(err)
-  //         })
-  //     }
-  // })
-  // .catch((err) => {
-  //     throw err
-  // })
-  // console.log("all orders: " + allOrders)
+    .then((response) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.send(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
